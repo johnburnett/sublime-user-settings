cdb5de40c5bf6a96a5c7bf941d9a3f519db743cb
John Burnett <jburnett@blizzard.com>
2011-08-05 22:39:01 -0700

    Adding open_file_browser_here

--- open_shell_here.py
+++ open_shell_here.py
@@ -7,22 +7,27 @@ import sublime, sublime_plugin
 class open_shell_here(sublime_plugin.TextCommand):
 
     def run(self, edit):
-        dir_path = self.get_dir()
+        dir_path, file_path = self.get_dir_and_file()
         if dir_path:
-            self.open_shell(dir_path)
+            self.open_shell(dir_path, file_path)
 
     def is_enabled(self):
-        return bool(self.get_dir())
+        return bool(self.get_dir_and_file()[0])
 
-    def get_dir(self):
-        filename = self.view.file_name()
-        if filename:
-            dir_path = os.path.dirname(filename)
+    def get_dir_and_file(self):
+        """Returns (dir_path, file_path) tuple.  Either component may be
+        None if that component doesn't exist on disk."""
+        file_path = self.view.file_name()
+        if file_path:
+            dir_path = os.path.dirname(file_path)
             if os.path.isdir(dir_path):
-                return dir_path
-        return None
+                if os.path.isfile(file_path):
+                    return dir_path, file_path
+                else:
+                    return dir_path, None
+        return None, None
 
-    def open_shell(self, dir_path):
+    def open_shell(self, dir_path, file_path):
         raise NotImplemented()
 
 
@@ -31,7 +36,7 @@ class open_cmd_here(open_shell_here):
     def is_enabled(self):
         return sys.platform == 'win32' and super(self.__class__, self).is_enabled()
 
-    def open_shell(self, dir_path):
+    def open_shell(self, dir_path, file_path):
         subprocess.Popen(['cmd.exe', '/k', 'pushd', dir_path], shell=False)
 
 
@@ -40,9 +45,22 @@ class open_bash_here(open_shell_here):
     def is_enabled(self):
         return sys.platform == 'win32' and super(self.__class__, self).is_enabled()
 
-    def open_shell(self, dir_path):
+    def open_shell(self, dir_path, file_path):
         cmd_path = os.path.join(os.environ.get('SystemRoot', ''), r'SysWOW64\cmd.exe')
         bash_path = os.path.join(os.environ.get('ProgramFiles(x86)', ''), r'Git\bin\sh.exe')
 
         cmd = '%s /c "pushd "%s" && "%s" --login -i"' % (cmd_path, dir_path, bash_path)
         subprocess.Popen(cmd)
+
+
+class open_file_browser_here(open_shell_here):
+
+    def is_enabled(self):
+        return sys.platform == 'win32' and super(self.__class__, self).is_enabled()
+
+    def open_shell(self, dir_path, file_path):
+        explorer = os.path.join(os.environ['SYSTEMROOT'], 'explorer.exe')
+        if file_path:
+            subprocess.Popen([explorer, '/select,', file_path])
+        else:
+            subprocess.Popen([explorer, dir_path])
