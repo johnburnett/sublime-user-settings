34b0e8039f6c6e16b95203cf49a8fc8f637ee4e3
John Burnett <jburnett@blizzard.com>
2011-09-02 15:01:33 -0700

    Adding 'Toggle Command Logging'

---
+++ dev_tools.py
@@ -0,0 +1,18 @@
+import sublime, sublime_plugin
+
+class toggle_command_logging(sublime_plugin.WindowCommand):
+    def __init__(self, window):
+        super(toggle_command_logging, self).__init__(window)
+        self.logEnabled = False
+
+    def run(self):
+        self.logEnabled = not self.logEnabled
+        sublime.log_commands(self.logEnabled)
+        if self.logEnabled:
+            self.window.run_command('show_panel', {'panel': 'console'} )
+        else:
+            self.window.run_command('hide_panel', {'panel': 'console'} )
+
+    def description(self):
+        state = 'Disable' if self.logEnabled else 'Enable'
+        return state + ' Command Logging'
