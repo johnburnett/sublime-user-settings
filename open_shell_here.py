d65c5967a6d0da582795fabef38ae4ad53456991
John Burnett <jburnett@blizzard.com>
2011-12-14 18:12:42 -0800

    Derive from ApplicationCommand now, so open_shell can run when no file is open.  Defaults to user home if that's the case.  Also re-adds open_file_browser_here, to support this new behavior.

--- open_shell_here.py
+++ open_shell_here.py
@@ -13,30 +13,41 @@ def open_bash(dir_path):
     subprocess.Popen(cmd)
 
 
-class open_shell_here(sublime_plugin.TextCommand):
+class open_shell_here(sublime_plugin.ApplicationCommand):
 
-    def run(self, edit):
-        dir_path, file_path = self.get_dir_and_file()
+    def run(self):
+        dir_path, file_name = self.get_dir_and_file()
         if dir_path:
-            self.open_shell(dir_path, file_path)
+            self.open_shell(dir_path, file_name)
 
     def is_enabled(self):
         return bool(self.get_dir_and_file()[0])
 
     def get_dir_and_file(self):
-        """Returns (dir_path, file_path) tuple.  Either component may be
-        None if that component doesn't exist on disk."""
-        file_path = self.view.file_name()
+        """Returns (dir_path, file_name) tuple.  file_name may be None."""
+
+        file_path = None
+        win = sublime.active_window()
+        if win:
+            view = win.active_view()
+            if view:
+                file_path = view.file_name()
+
+        dir_path = None
+        file_name = None
         if file_path:
             dir_path = os.path.dirname(file_path)
-            if os.path.isdir(dir_path):
-                if os.path.isfile(file_path):
-                    return dir_path, file_path
-                else:
-                    return dir_path, None
-        return None, None
+            if os.path.isfile(file_path):
+                file_name = os.path.basename(file_path)
 
-    def open_shell(self, dir_path, file_path):
+        if not dir_path or not os.path.isdir(dir_path):
+            dir_path = os.path.expanduser('~')
+            if sys.platform == 'win32':
+                dir_path = os.environ.get('USERPROFILE', dir_path)
+
+        return dir_path, file_name
+
+    def open_shell(self, dir_path, file_name):
         raise NotImplemented()
 
 
@@ -45,8 +56,22 @@ class open_cmd_here(open_shell_here):
     def is_enabled(self):
         return sys.platform == 'win32' and super(open_cmd_here, self).is_enabled()
 
-    def open_shell(self, dir_path, file_path):
+    def open_shell(self, dir_path, file_name):
         subprocess.Popen(['cmd.exe', '/k', 'pushd', dir_path], shell=False)
+
+
+class open_file_browser_here(open_shell_here):
+
+    def is_enabled(self):
+        return sys.platform == 'win32' and super(self.__class__, self).is_enabled()
+
+    def open_shell(self, dir_path, file_name):
+        explorer = os.path.join(os.environ['SYSTEMROOT'], 'explorer.exe')
+        if file_name:
+            file_path = os.path.join(dir_path, file_name)
+            subprocess.Popen([explorer, '/select,', file_path])
+        else:
+            subprocess.Popen([explorer, dir_path])
 
 
 class open_bash_here(open_shell_here):
@@ -54,7 +79,7 @@ class open_bash_here(open_shell_here):
     def is_enabled(self):
         return sys.platform == 'win32' and super(open_bash_here, self).is_enabled()
 
-    def open_shell(self, dir_path, file_path):
+    def open_shell(self, dir_path, file_name):
         open_bash(dir_path)
 
 
