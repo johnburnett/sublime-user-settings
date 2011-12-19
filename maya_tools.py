75b91253f2c1927343a7035fc99699fd012d0112
John Burnett <jburnett@blizzard.com>
2011-12-19 14:25:10 -0800

    Adds support for eval'ing selected text only.

--- maya_tools.py
+++ maya_tools.py
@@ -28,18 +28,14 @@ class eval_in_maya(sublime_plugin.TextCommand):
         # If current view is not dirty, use that file, otherwise use a temp file that
         # will be cleaned up after running.
 
-        temp_file_path = None
-        if not self.view.file_name() or self.view.is_dirty():
-            temp_file_path = self.write_temp_file()
-        eval_file_path = temp_file_path if temp_file_path else self.view.file_name()
-        eval_file_path = eval_file_path.replace('\\', '/')
+        eval_file_path, is_temp_file = self.get_eval_file_path()
 
         if syntax == 'python':
             mel_str = k_evalpy_mel_str % eval_file_path
         elif syntax == 'mel':
             mel_str = 'catch(eval("source\\"%s\\""))' % eval_file_path
 
-        if temp_file_path:
+        if is_temp_file:
             # Make Maya delete the file itself to avoid race condition of
             # deleting the file before Maya has read it.
             mel_str += ';sysFile -delete "%s"' % eval_file_path
@@ -55,15 +51,14 @@ class eval_in_maya(sublime_plugin.TextCommand):
         except:
             sublime.error_message("Error sending command to Maya.\n\nSee Sublime console for details.")
             traceback.print_exc()
-            if temp_file_path:
-                os.remove(temp_file_path)
+            if is_temp_file:
+                os.remove(eval_file_path)
         finally:
             if sock:
                 sock.close()
 
 
     def get_file_syntax(self):
-        #todo: test with mel file and mel syntax addon.......?
         syntax, _ = os.path.splitext(os.path.basename(self.view.settings().get('syntax')))
         syntax = syntax.lower()
         if syntax == 'plain text':
@@ -76,16 +71,37 @@ class eval_in_maya(sublime_plugin.TextCommand):
         return syntax if syntax in ('python', 'mel') else None
 
 
-    def write_temp_file(self):
-        all_text_region = sublime.Region(0, self.view.size())
-        all_text = self.view.substr(all_text_region)
+    def get_eval_file_path(self):
+        """Returns a tuple of (filepath, is_temp_file)"""
+
+        is_temp = False
+        eval_regions = [r for r in self.view.sel() if not r.empty()]
+        if eval_regions:
+            region_text = u''
+            for r in eval_regions:
+                region_text += self.view.substr(r)
+            # todo: remove trailing common whitespace, if any
+            eval_file_path = self.write_temp_file(region_text)
+            is_temp = True
+        else:
+            if self.view.file_name() and not self.view.is_dirty():
+                eval_file_path = self.view.file_name()
+            else:
+                all_text = self.view.substr(sublime.Region(0, self.view.size()))
+                eval_file_path = self.write_temp_file(all_text)
+                is_temp = True
+
+        eval_file_path = eval_file_path.replace('\\', '/')
+        return eval_file_path, is_temp
+
+
+    def write_temp_file(self, txt):
         fileInfo = tempfile.mkstemp(prefix="eval_in_maya_temp_", text=True)
         if not fileInfo:
             sublime.error_message("Can't create temp file")
             return None
         try:
-            os.write(fileInfo[0], all_text)
+            os.write(fileInfo[0], txt)
         finally:
             os.close(fileInfo[0])
         return fileInfo[1]
-
