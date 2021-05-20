d982bcb5aec33f2189403a4f7f7d77a3e98c1378
John Burnett <github@johnburnett.com>
2021-05-20 14:35:06 -0700

    Add rename_buffer

---
+++ rename_buffer.py
@@ -0,0 +1,23 @@
+import sublime
+import sublime_plugin
+
+
+class RenameBuffer(sublime_plugin.TextCommand):
+    def is_enabled(self):
+        return self.view.file_name() is None
+
+    def run(self, edit):
+        # Prevent the user from attempting to
+        # change the name of a panel or overlay.
+        if self.view.settings().get("is_widget"):
+            msg = "run from an unsaved buffer not a panel or overlay"
+            sublime.status_message(msg)
+        else:
+            msg =  "Buffer name:"
+            self.view.window().show_input_panel(msg, "", self.on_panel_done, None, None)
+
+    def on_panel_done(self, buffer_name):
+        buffer_name = buffer_name.strip()
+        if not buffer_name:
+            return
+        self.view.set_name(buffer_name)
